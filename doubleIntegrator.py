Found a little mistake with covariance matrices, now solved.

doubleIntegrator.py
@@ -12,6 +12,21 @@ import numpy as np
 import scipy.linalg
 import matplotlib.pyplot as plt
 
+### define font size for plots ###
+#
+SMALL_SIZE = 16
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 22
+
+plt.rc('font', size=MEDIUM_SIZE)            # controls default text sizes
+plt.rc('axes', titlesize=MEDIUM_SIZE)       # fontsize of the axes title
+plt.rc('axes', labelsize=SMALL_SIZE)        # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)       # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)       # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)       # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)     # fontsize of the figure title
+#
+
 plt.close('all')
 dt = .01
 T = 15
@@ -33,7 +48,7 @@ def doubleInt():
     
     A = np.array([[0, 1], [0, 0]])               # state transition matrix
     B = np.array([[0], [1]])                     # input matrix
-    C = np.array([[0, 0], [0, 1]])               # dynamics noise matrix
+    C = .1 * np.array([[0, 0], [0, 1]])               # dynamics noise matrix
     D = np.array([[1, 0], [0, 1]])               # observations noise matrix
     H = np.array([[1, 0], [0, 1]])               # measurement matrix
     
@@ -42,7 +57,7 @@ def doubleInt():
     
     # controller
     Q = np.array([[1, 0], [0, 1]])
-    R = 2
+    R = 4
     
     x_hat = np.zeros((variables, 1))
     x_dot_hat = np.zeros((variables, 1))
@@ -52,8 +67,8 @@ def doubleInt():
     L = np.zeros((variables, variables))
     
     # estimator
-    Z = .1*np.array([[1, 0], [0, 1]])
-    W = np.array([[1, 0], [0, 1]])
+#    Z = .1*np.array([[1, 0], [0, 1]])
+#    W = np.array([[1, 0], [0, 1]])
     
     P = np.zeros((iterations, variables, variables))
     P_dot = np.zeros((variables, variables))
@@ -73,8 +88,8 @@ def doubleInt():
     x = 10 * np.random.rand(variables, 1) - 5
     x_hat = x + .1 * np.random.rand(variables, 1)
     
-    # use Riccati equations solver in scipy
-    #P = scipy.linalg.solve_continuous_are(A.transpose(), H.transpose(), np.dot(W, W.transpose()), np.dot(Z, Z.transpose()))
+    # use Riccati equations solver in scipy, assuming the system is LTI
+    #P = scipy.linalg.solve_continuous_are(A.transpose(), H.transpose(), np.dot(C, C.transpose()), np.dot(D, D.transpose()))
     #V = scipy.linalg.solve_continuous_are(A, B, Q, R)
     
     for i in range(iterations-1, 1, -1):
@@ -92,11 +107,8 @@ def doubleInt():
         # control dynamics
         # (estimate state for output feedback)
         x_dot_hat = np.dot(A, x_hat) + np.dot(B, u) + np.dot(K, (y - np.dot(H, x_hat)))
-        P_dot = np.dot(A, P[i, :, :]) + np.dot(P[i, :, :], A.transpose()) + W - np.dot(K, np.dot(Z, K.transpose()))
-        K = np.dot(P[i, :, :], np.dot(H.transpose(), np.linalg.inv(np.dot(Z, Z.transpose()))))
-        
-    #    K[0, 1] = 0.
-    #    K[1, 0] = 0.
+        P_dot = np.dot(A, P[i, :, :]) + np.dot(P[i, :, :], A.transpose()) + np.dot(C, C.transpose()) - np.dot(K, np.dot(np.dot(D, D.transpose()), K.transpose()))
+        K = np.dot(P[i, :, :], np.dot(H.transpose(), np.linalg.inv(np.dot(D, D.transpose()))))
         
         x_hat += dt * x_dot_hat
         P[i+1, :, :] = P[i, :, :] + dt * P_dot
@@ -123,6 +135,7 @@ y_history = np.zeros((simulations_n, iterations, variables, 1))
 x_hat_history = np.zeros((simulations_n, iterations, variables, 1))
 
 plt.figure(figsize=(9, 6))
+plt.title('Double integrator - LQG')
 plt.xlabel('Position ($m$)')
 plt.ylabel('Velocity ($m/s$)')
 for k in range(simulations_n):